487b0a4a26b263e147822e6a3cd3d5ad0f4851c1
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2025-04-20 02:16:08 +0530

    Add GraphQL API route: implement yoga server with schema and resolvers

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,14 @@
+import { createYoga, createSchema } from 'graphql-yoga';
+import { type NextRequest } from 'next/server';
+import resolvers from '@/graphql/resolvers';
+import typeDefs from '@/graphql/schema';
+
+const yoga = createYoga<{
+  req: NextRequest;
+}>({
+  schema: createSchema({ typeDefs, resolvers }),
+  graphqlEndpoint: '/api/graphql',
+  fetchAPI: { Request, Response },
+});
+
+export { yoga as GET, yoga as POST };
