ae9bd07839b38ca90e441c17444a72f2c973566e
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2025-04-20 02:17:17 +0530

    Add Todo model: implement Mongoose schema and interface for Todo management

---
+++ models/todo.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface ITodo extends Document {
+  title: string;
+  completed: boolean;
+}
+
+const todoSchema = new Schema<ITodo>(
+  {
+    title: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.models.Todo || mongoose.model<ITodo>('Todo', todoSchema);
+
+export default Todo;
