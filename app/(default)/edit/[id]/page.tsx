81c5edb4f373387efb85ffd396422c13c88fdd77
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2025-04-26 20:20:47 +0530

    Refactor TodoEditPage component: update parameter type to Promise and improve async handling for clarity and consistency.

--- app/(default)/edit/[id]/page.tsx
+++ app/(default)/edit/[id]/page.tsx
@@ -4,21 +4,20 @@ import type { TodoResponse } from '../../types';
 import EditTodo from './components/EditTodo';
 
 type PageProps = {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 };
 
 export default async function TodoEditPage({ params }: PageProps) {
-  const { id } = params;
+  const { id } = await params;
+
   const { data, error } = await fetchGraphQL<TodoResponse>(GET_TODO_BY_ID, {
     id,
   });
 
   return (
-    <>
+    <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Edit Todo</h1>
       <EditTodo data={data} error={error} />
-    </>
+    </div>
   );
 }
