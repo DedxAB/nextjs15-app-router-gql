27d611a383d763e17863ed3ae8acf3bc8a0168db
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2025-04-26 20:25:44 +0530

    Refactor constants: remove default value for GRAPHQL_ENDPOINT to ensure environment variable is explicitly defined.

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,2 @@
-export const GRAPHQL_ENDPOINT =
-  process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || '/api/graphql';
-
+export const GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
 export const MONGODB_URI = process.env.MONGODB_URI;
