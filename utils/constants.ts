593b3d6cd0d81f9e8e8ca2fd73ff6a02193e2185
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2025-04-28 01:03:45 +0530

    Add MONGODB_NAME constant to constants file for environment configuration

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,3 @@
 export const GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!;
 export const MONGODB_URI = process.env.MONGODB_URI!;
+export const MONGODB_NAME = process.env.MONGODB_NAME!;
